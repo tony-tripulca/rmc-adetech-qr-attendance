ce2e01bc9643f982396a39e220071758b4a9974e
tony-tripulca <tony.tripulca@gmail.com>
2025-12-20 21:59:44 +0800

    [feat] Sample crud operations

--- api/books.js
+++ api/books.js
@@ -1,4 +1,6 @@
 const bodyParser = require('../lib/body-parser');
+const parseUrl = require('../lib/url-parser');
+const { send404, send405 } = require('../lib/response-utils');
 
 const books = [
   { id: 1, title: 'Clean Code' },
@@ -6,29 +8,82 @@ const books = [
 ];
 
 module.exports = async function bookService(req, res) {
-  switch (req.method) {
-    // GET /api/books
-    case 'GET':
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(books));
-      return;
-    // POST /api/books
-    case 'POST':
-      try {
-        const data = await bodyParser(req);
-        books.push(data);
-        res.statusCode = 201;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data));
-      } catch (err) {
-        res.statusCode = 400;
-        res.end(err.message);
-      }
-      return;
-    default:
-      res.statusCode = 405;
-      res.setHeader('Allow', 'GET, POST');
-      res.end('Method Not Allowed');
-      return;
+  const parsed = parseUrl(req.url);
+
+  if (!parsed) {
+    send404(res);
+    return;
   }
-};
+
+  const { action, id } = parsed;
+
+  if (req.method !== 'GET' && req.method !== 'POST' && req.method !== 'PUT') {
+    send405(res, 'GET, POST, PUT');
+    return;
+  }
+
+  // GET /api/books/list
+  if (action === 'list') {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(books));
+    return;
+  }
+
+  // GET /api/books/read/:id
+  if (action === 'read' && id !== null) {
+    const book = books.find(b => b.id === id);
+
+    if (!book) {
+      send404(res);
+      return;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(book));
+    return;
+  }
+
+  // POST /api/books/create
+  if (action === 'create') {
+    try {
+      const data = await bodyParser(req);
+
+      books.push(data);
+
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(data));
+      return;
+
+    } catch (err) {
+      res.statusCode = 400;
+      res.end(err.message);
+      return;
+    }
+  }
+
+  // PUT /api/books/update/:id
+  if (action === 'update' && id !== null) {
+    const book = books.find(b => b.id === id);
+
+    if (!book) {
+      send404(res);
+      return;
+    }
+
+    try {
+      const data = await bodyParser(req);
+      books[index] = { ...books[index], ...data };
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(books[index]));
+      return;
+    } catch (err) {
+      res.statusCode = 400;
+      res.end(err.message);
+      return;
+    }
+  }
+
+  send404(res);
+  return;
+}
